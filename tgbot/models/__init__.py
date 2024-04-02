819917e3066aa2b49ad9bf65c62142b3036a3f36
dhmmmhb <dhmaguran@gmail.com>
2024-04-02 23:58:59 +0300

    tgbot.models.__init__.py bugfix update

--- tgbot/models/__init__.py
+++ tgbot/models/__init__.py
@@ -1,5 +1,6 @@
 import importlib
 import os
+from copy import deepcopy
 
 from misc.root import get_root, get_abspath
 
@@ -8,7 +9,7 @@ async def connect_models():
     path = get_abspath(__file__)
     python_models = os.listdir("/".join(path))
 
-    for el in python_models:
+    for el in deepcopy(python_models):
         if not el[-3:] == ".py":
             python_models.remove(el)
     python_models.remove("__init__.py")
